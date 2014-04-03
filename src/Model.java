a2c3307f4d77bb7fc49ac9698ddd23f0ebcc60c5
Gustav Svalander <gsvalander@gmail.com>
2014-04-03 14:26:57 +0200

    javadoc model

--- src/Model.java
+++ src/Model.java
@@ -11,144 +11,121 @@ import java.math.BigInteger;
 import java.net.URL;
 
 /**
- * The Model Class is where we make all calculations & and handle all the data.
- * We observe this object from the view.
- */
+* The Model Class is where we make all calculations & and handle all the data.
+* We observe this object from the view.
+*/
 class Model extends Observable {
 
-	private PaymentProvider paymentProvider;
-	private BtcMarket btcMarket;
-	private double currentExchangeRate = -1.0;
+  private PaymentProvider paymentProvider;
+  private BtcMarket btcMarket;
+  private double currentExchangeRate = -1.0;
 
-	Model(){
-		System.out.println(">> Model.Model()");
-		paymentProvider = new PaymentProvider();
-		btcMarket = new BtcMarket();
-	}
-	public boolean checkUser (String username, String password) throws Exception {
-        String line = null;
+  Model(){
+    System.out.println(">> Model.Model()");
+    paymentProvider = new PaymentProvider();
+    btcMarket = new BtcMarket();
+  }
+  /**
+   * Check if user is registered within our system
+   *
+   * @param The users username
+   * @param The users password
+   */
+  public boolean checkUser (String username, String password) throws Exception {
+    String line = null;
 
 
-        BufferedReader reader = new BufferedReader(new FileReader("account.txt"));
+    BufferedReader reader = new BufferedReader(new FileReader("account.txt"));
 
-        while((line=reader.readLine())!=null){
+    while((line=reader.readLine())!=null){
 
-            if(line.equals(username)){
-                line=reader.readLine();
+      if(line.equals(username)){
+        line=reader.readLine();
 
-                if (line.equals(password)){
-                	reader.close();
-                    return true;
-                }
-
-                else{
-                	reader.close();
-                    return false;
-                }
-
-            }
-
+        if (line.equals(password)){
+          reader.close();
+          return true;
         }
+
+        else{
+          reader.close();
+          return false;
+        }
+
+      }
+
+    }
+    reader.close();
+    return false;
+  }
+
+  /**
+   * This method returns a hashed password of the users password.
+   */
+  public String hashPassword(String password)
+  {
+    System.out.println(">> Model.hashPassword("+password+")");
+    BigInteger hash = null;
+    for(int i=0; i<1338; i++){
+      try {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(password.getBytes());
+
+        hash = new BigInteger(1, md5.digest());
+
+        password = hash.toString(16);
+
+      } catch (NoSuchAlgorithmException nsae) {
+        // ignore
+      }
+      while(password.length() < 32 ){
+        password = "0"+password;
+      }
+    }
+    return password;
+  }
+
+  /**
+   * This method updates the exchange rate from a server and updates the label
+   * in the View.
+   */
+  public void updateExchangeRate(){
+    btcMarket.updateRate();
+    setChanged();
+    notifyObservers(btcMarket);
+  }
+  /**
+   * Create a new user using username and password
+   *
+   * @param users desired username
+   * @param users desired password
+   * @throws Exception if we cannot open file
+   */
+  public boolean newUser(String username, String password)throws Exception{
+    String line = null;
+
+    BufferedReader reader = new BufferedReader(new FileReader("account.txt"));
+    BufferedWriter writer = new BufferedWriter(new FileWriter("account.txt", true));
+    //PrintWriter writer = new PrintWriter ("account.txt");
+
+    while((line=reader.readLine())!=null){
+      System.out.println(line);
+
+      if(line.equals(username)){
+        writer.close();
         reader.close();
+        setChanged();
+        notifyObservers("cannot create user");
+
         return false;
-    }
-
-	/*
-	 * This method checks if the email & password is correct when the user loggin by
-	 * match the email & a hashed password against a textfile
-	 */
-	/*public boolean checkUser(String username, String password){
-		System.out.println(">> Model.checkUser("+username+")");
-		return checkUser(username, password);
-
-		if(email.equals("admin") && password.equals(hashPassword("test"))){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	/*
-	/*
-	 * This method returns a hashed password of the users password.
-	 */
-	public String hashPassword(String password)
-    {
-		System.out.println(">> Model.hashPassword("+password+")");
-        BigInteger hash = null;
-        for(int i=0; i<1338; i++){
-            try {
-                MessageDigest md5 = MessageDigest.getInstance("MD5");
-                md5.update(password.getBytes());
-
-                hash = new BigInteger(1, md5.digest());
-
-                password = hash.toString(16);
-
-            } catch (NoSuchAlgorithmException nsae) {
-                // ignore
-            }
-            while(password.length() < 32 ){
-                password = "0"+password;
-            }
-        }
-        return password;
-    }
-
-	// Called from controller
-	/*public void action(Object arg, String currency){
-		System.out.println(">> Model.action()");
-		//........Updates view...........
-		//Updates View in transactionwindow for exchangerates
-		if(arg instanceof Double){
-			if(currency.equals("SEK")){
-				//notifyObservers(btcMarkets[0].getExchangeRateForSEK((Double)arg));
-			}
-			else{
-				//notifyObservers...getExchangeRateForBTC
-			}
-		}
-	}*/
-
-	/*
-	 * This method updates the exchange rate from a server and updates the label
-	 * in the View.
-	 */
-	public void updateExchangeRate(){
-		btcMarket.updateRate();
-		setChanged();
-		notifyObservers(btcMarket);
-	}
-	public boolean newUser(String username, String password)throws Exception{
-        String line = null;
-
-
-        BufferedReader reader = new BufferedReader(new FileReader("account.txt"));
-        BufferedWriter writer = new BufferedWriter(new FileWriter("account.txt", true));
-        //PrintWriter writer = new PrintWriter ("account.txt");
-
-        while((line=reader.readLine())!=null){
-        	System.out.println(line);
-
-            if(line.equals(username)){
-            	writer.close();
-            	reader.close();
-            	setChanged();
-        		notifyObservers("cannot create user");
-
-            	return false;
-            }
-
-        }
-
-
-
-            writer.write(username);
-            writer.newLine();
-            writer.write(password);
-            writer.newLine();
-            writer.close();
-            return true;
-
+      }
 
     }
+    writer.write(username);
+    writer.newLine();
+    writer.write(password);
+    writer.newLine();
+    writer.close();
+    return true;
+  }
 }
